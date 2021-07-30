310e3a76e1d8f2253b51f8ff6c6421dc0e5504f8
Fran Canete <francanetedev@gmail.com>
2021-07-30 18:06:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 const startButton = document.querySelector('.btn__reset');
-const qwerty = document.querySelector('.qwerty').querySelectorAll('button');
+const overlay = document.getElementById('overlay');
+const keys = document.querySelectorAll('button');
 
+const header = document.querySelector('.header');
+
+
+
+startButton.addEventListener('click', () => {
+    overlay.style.display = 'none';
+});
+
+
+keys.forEach( key => key.addEventListener('click', function () {
+    header.style.color = 'red';
+    console.log(key);
+}));
