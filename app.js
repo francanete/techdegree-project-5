6f5fa79d18c8646396a0928d58129ae6d2a0c407
Fran Canete <francanetedev@gmail.com>
2021-07-31 23:55:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const startButton = document.querySelector('.btn__reset');
 const overlay = document.getElementById('overlay');
 const keys = document.querySelectorAll('button');
+const phrase = document.querySelector('#phrase');
+
+let missed = 0;
+
+let phrases = [
+    'phrase number 1',
+    'phrase number 2',
+    'phrase number 3',
+    'phrase number 4',
+    'phrase number 5'
+];
 
 const header = document.querySelector('.header');
 
